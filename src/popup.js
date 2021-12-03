fcf44cb91822d951af4c5a6146e1fd3e3614a9d3
Mola19 <Mola9041@gmail.com>
2021-12-04 00:41:59 +0100

    rewrite and modernize code

--- src/popup.js
+++ src/popup.js
@@ -1,66 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
 
-  // Chrome has a hardcoded popup window size limit of 800x600
-  var max_window_height = 600;
-  var max_qrcode_height = max_window_height - 75; // Reserve "some" space for UI
+	chrome.tabs.getSelected(null, (tab) => {
+		document.getElementById("textbox").value = tab.url
+		load_qr()
+	})
 
-  var qr_levels = ["M", "L"];
-  var qr_modules_by_version = {
-    1: 21, 2: 25, 3: 29, 4: 33, 5: 37,
-    6: 41, 7: 45, 8: 49, 9: 53, 10: 57
-  }
+	document.getElementById("close").onclick = () => {
+		window.close()
+	}
 
-  var createImage = function(payload) {
-    var qr_margin = 4;
+	document.getElementById("textbox").onchange = () => {
+		load_qr()
+	}
 
-    for (var levelIndex in qr_levels) {
-      for (var typeNum = 1; typeNum <= 10; typeNum++) {
-        var qr_cellsize = Math.floor(max_qrcode_height / qr_modules_by_version[typeNum]);
-        try {
-          var qr = qrcode(typeNum, qr_levels[levelIndex]);
-          qr.addData(payload);
-          qr.make();
-          return qr.createImgTag(qr_cellsize, qr_margin);
-        } catch(e) {
-          if (strStartsWith(e.message, "code length overflow")) {
-            // ignore and try to use bigger QR code format
-          } else {
-            throw e;
-          }
-        }
-      }
-    }
-  };
+	document.getElementById("textbox").onkeyup = () => {
+		load_qr()
+	}
 
-  var updateImage = function() {
-    payload = document.getElementById("textbox").value;
-    document.getElementById("insert-qrcode-here").innerHTML =
-      createImage(payload) || "Error. URL too long?";
-  };
+})
 
-  var strStartsWith = function(string, prefix) {
-    return !string.indexOf(prefix);
-  };
+function load_qr () {
+	//todo size rework
+	let max_window_height = 600
+	let max_qrcode_height = max_window_height - 75 // Reserve "some" space for UI
 
-  document.getElementById("close").onclick = function() {
-    window.close();
-  };
-
-  document.getElementById("textbox").onchange = function() {
-    updateImage();
-  };
-
-  document.getElementById("textbox").onkeyup = function() {
-    updateImage();
-  };
-
-  document.getElementById("textbox").onclick = function() {
-    this.select();
-  };
-
-  chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById("textbox").value = tab.url;
-    document.getElementById("textbox").select();
-    updateImage();
-  });
-});
+	let qr = qrcode(0, "M")
+	qr.addData(document.getElementById("textbox").value)
+	qr.make()
+	document.getElementById("qrcode").innerHTML = qr.createImgTag(Math.floor(max_qrcode_height / 57), 4)
+}
